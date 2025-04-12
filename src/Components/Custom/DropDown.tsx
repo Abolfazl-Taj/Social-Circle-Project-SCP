f6d2bd3552538c75aee2919e524f9b89baa3b706
Abolfazl Taj <103893533+Abolfazl-Taj@users.noreply.github.com>
2025-04-12 15:31:09 +0330

    Add files via upload

--- src/Components/Custom/DropDown.tsx
+++ src/Components/Custom/DropDown.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Status } from "../../Assets/Data/Status_Data";
+import { IoTrashBin } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const DropDown = ({
   data,
@@ -11,63 +13,100 @@ const DropDown = ({
   name?: string;
 }) => {
   const [isOpen, setIsopen] = useState(false);
-  const [showselected, setShowselected] = useState<any>(
-    "Please Select The Status"
-  );
-  const SelectHandler = (
-    e: React.MouseEvent<HTMLDivElement>,
-    value: string
-  ) => {
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+
+  const SelectHandler = (e: React.MouseEvent<HTMLDivElement>, item: Status) => {
     const mockInput = {
       target: {
         name,
-        value,
+        value: item.icon,
       },
     };
     formAction(mockInput);
     e.stopPropagation();
-    const selecteditem = (e.currentTarget as HTMLElement).outerHTML;
-    setShowselected(selecteditem);
+    setSelectedStatus(item);
+    setIsopen(false);
   };
+
   const clearSelect = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
-    setShowselected("Please Select A Status");
+    setIsopen(false);
+    setSelectedStatus(null);
   };
+
   return (
     <div
       onClick={() => setIsopen(!isOpen)}
-      className={`my-4 w-full outline-noone border  border-slate-700  rounded shadow-2xl overflow-hidden transition-all px-4 py-6  ${
-        isOpen ? "max-h-max" : "max-h-16 "
-      }}`}
+      className={`my-4 w-full outline-noone border border-slate-700 rounded shadow-2xl overflow-hidden transition-all duration-700 ease-in-out
+        ${
+          isOpen
+            ? "max-h-[600px] px-4 py-6"
+            : selectedStatus
+            ? "max-h-[140px] px-4 py-6"
+            : "max-h-[50px] "
+        }`}
     >
       <div>
-        <h5>Value Selected</h5>
-
-        {showselected && (
-          <div
-            onClick={clearSelect}
-            className="my-4 border rounded px-4 py-2 border-slate-600 bg-gradient-to-r from-slate-900 to-slate-500 "
-            dangerouslySetInnerHTML={{ __html: showselected }}
-          />
+        {selectedStatus && (
+          <>
+            <h5
+              className="text-slate-400 font-medium w-full
+              text-center flex items-center justify-center"
+            >
+              Value Selected
+              <span
+                className={`text-2xl transition-all duration-700 ease-in-out ${
+                  isOpen ? "rotate-180" : ""
+                }`}
+              >
+                <IoMdArrowDropdown />
+              </span>
+            </h5>
+            <div
+              className="my-4 border rounded px-4 py-1 border-slate-600 bg-gradient-to-r from-slate-900 to-slate-500 flex justify-between items-center"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="flex items-center">
+                <i className="mx-1 text-[50px]">
+                  <selectedStatus.icon />
+                </i>
+                <p>{selectedStatus.text}</p>
+              </div>
+              <div
+                className="text-[30px] text-slate-100 hover:text-red-950 transition-all"
+                onClick={clearSelect}
+              >
+                <IoTrashBin />
+              </div>
+            </div>
+          </>
+        )}
+        {!selectedStatus && (
+          <p className="text-slate-400 font-medium my-3 w-full  text-center flex items-center justify-center">
+            Please Select A Status
+            <span
+              className={`text-2xl transition-all duration-700 ease-in-out ${
+                isOpen ? "rotate-180" : ""
+              }`}
+            >
+              <IoMdArrowDropdown />
+            </span>
+          </p>
         )}
       </div>
-      {data.map((item: Status) => {
-        return (
-          <div
-            className={` hover:!bg-slate-700 !outline-0 flex py-1 px-4 items-center rounded ${
-              isOpen ? "flex" : "hidden"
-            }`}
-            key={item.id}
-            name={item.text}
-            onClick={(e) => SelectHandler(e, item.text)}
-          >
-            <i className="mx-1 text-[40px]">
-              <item.icon />
-            </i>
-            <p>{item.text}</p>
-          </div>
-        );
-      })}
+
+      {data.map((item: Status) => (
+        <div
+          className="hover:bg-slate-700 outline-0 flex py-1 px-4 items-center  cursor-pointer transition ease-in-out border my-2 border-slate-800 shadow rounded"
+          key={item.id}
+          onClick={(e) => SelectHandler(e, item)}
+        >
+          <i className="mx-1 text-[50px]">
+            <item.icon />
+          </i>
+          <p>{item.text}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,40 @@
-import Continer from "../../Components/Custom/Continer"
+import { useEffect, useState } from "react";
+import Continer from "../../Components/Custom/Continer";
+import axios from "axios";
+import { Status } from "../../Assets/Data/Status_Data";
 
 const Home = () => {
+  const [persons, setpersons] = useState([]);
+  interface person {
+    id: string;
+    Full_Name: string;
+    Brith_Day: string;
+    Description: string;
+    Status: Status;
+  }
+  useEffect(() => {
+    axios.get("http://localhost:3000/person").then((res) => {
+      setpersons(res.data);
+    });
+  });
   return (
     <Continer>
-      <div className="font-bold">
-        Hello
+      <div className="my-4 font-bold bg-slate-800 px-5 py-12 rounded shadow flex items-center">
+        {persons &&
+          persons.map((person: person) => (
+            <div
+              className="w-1/3 bg-gradient-to-tl from-slate-600 to-slate-800 px-4 py-12 mx-4 rounded-xl shadow"
+              key={person.id}
+            >
+              {person.Full_Name}
+              {new Date().getFullYear() - Number(person.Brith_Day)}
+              {person.Description}
+              {person.Status.text}
+            </div>
+          ))}
       </div>
     </Continer>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
